85e026ca24e00118aa2e6f566da971c4bf0c3e49
ahmed mostafa saad <119954278+ahmedmostafa56389@users.noreply.github.com>
2024-07-24 03:02:26 +0300

    upload files

--- variables.tf
+++ variables.tf
@@ -1,3 +1,34 @@
+variable "public_subnets_names" {
+  type = list
+  description = "public subnets names"
+  default = ["public_1" , "public_2" ]
+}
+
+variable "private_subnets_names" {
+  type = list 
+  description = "private subnets names"
+  default = ["private_1" , "private_2"]
+}
+
+variable "public_cidrs" {
+  type = list
+  description = "public cidrs"
+  default = ["10.0.2.0/24" , "10.0.3.0/24"]
+}
+
+variable "private_cidrs" {
+  type = list 
+  description = "private cidrs"
+  default = ["10.0.0.0/24" , "10.0.1.0/24"]
+}
+
+variable "vpc_cidr" {
+  type = string 
+  description = "vpc cidr block"
+  default = "10.0.0.0/16"
+}
+
+
 variable "a_ami" {
   type = string 
   default = "ami-04a81a99f5ec58529"
